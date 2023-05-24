92aaf24eacd8cc29886cab1ef8f7d0730cc22aa4
Wojciech Sikora <wsikora@vuestorefront.io>
2023-05-24 14:00:56 +0200

    feat: add api-extractor-config

---
+++ packages/jest-config/src/index.js
@@ -0,0 +1,12 @@
+// For a detailed explanation regarding each configuration property, visit:
+// https://jestjs.io/docs/en/configuration.html
+
+export const baseConfig = {
+  preset: 'ts-jest',
+  transform: {
+    '^.+\\.(j|t)s$': 'ts-jest'
+  },
+  coverageDirectory: './coverage/',
+  collectCoverageFrom: ['src/**/*.ts'],
+  testMatch: ['<rootDir>/**/__tests__/**/*spec.[jt]s?(x)']
+};
